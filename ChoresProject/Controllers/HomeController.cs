7ffc862ceb82863654fae2a0597f2dbe6c4495e7
RobertJMitchell <rjrocks32@gmail.com>
2015-03-20 15:00:38 -0500

    base project files sync first time to github

---
+++ ChoresProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChoresProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Kitchen()
+        {
+            ViewBag.Message = "Welcome to the Kitchen Page";
+
+            return View();
+        }
+
+        public ActionResult Bedroom()
+        {
+            ViewBag.Message = "Welcome to the Bedroom (Page)";
+
+            return View();
+        }
+    }
+}
